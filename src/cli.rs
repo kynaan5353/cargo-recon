b5e413be4f8118009882df5ecbf4a937d693eb58
jmcph4 <jack@jmcph4.dev>
2025-03-31 12:17:58 +1000

    Add subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,27 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Clone, Debug, Parser)]
 #[clap(about, author, version)]
 pub struct Opts {
-    pub path: Option<PathBuf>,
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Clone, Debug, Subcommand)]
+pub enum Commands {
+    /// Lists viable fuzzing targets
+    #[command(arg_required_else_help = true)]
+    List {
+        /// Path to Rust code to search
+        path: Option<PathBuf>,
+    },
+    /// Write fuzzing tests
+    Generate {
+        /// Path to Rust code to search
+        inpath: Option<PathBuf>,
+        /// Path to write generated fuzzing tests to
+        outpath: Option<PathBuf>,
+    },
 }
--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,51 @@ use eyre::eyre;
 use search::search_file;
 use walkdir::WalkDir;
 
+use crate::cli::Commands;
+
 pub mod cli;
 pub mod search;
 
 fn main() -> eyre::Result<()> {
     let opts = Opts::parse();
 
-    let mut targets = Vec::new();
+    match opts.command {
+        Commands::List { path } => {
+            let mut targets = Vec::new();
 
-    let path = match opts.path {
-        Some(p) => p,
-        None => {
-            let mut p = PathBuf::new();
-            p.push(".");
-            p
-        }
-    };
+            let path = match path {
+                Some(p) => p,
+                None => {
+                    let mut p = PathBuf::new();
+                    p.push(".");
+                    p
+                }
+            };
 
-    if path.is_dir() {
-        for entry in WalkDir::new(&path).into_iter().filter_map(|e| e.ok()) {
-            if entry.file_type().is_file()
-                && entry.path().extension().and_then(|s| s.to_str())
-                    == Some("rs")
-            {
-                targets.extend(search_file(entry.path()));
+            if path.is_dir() {
+                for entry in
+                    WalkDir::new(&path).into_iter().filter_map(|e| e.ok())
+                {
+                    if entry.file_type().is_file()
+                        && entry.path().extension().and_then(|s| s.to_str())
+                            == Some("rs")
+                    {
+                        targets.extend(search_file(entry.path()));
+                    }
+                }
+            } else if path.is_file() {
+                targets.extend(search_file(&path));
+            } else {
+                return Err(eyre!("Not file nor directory"));
             }
-        }
-    } else if path.is_file() {
-        targets.extend(search_file(&path));
-    } else {
-        return Err(eyre!("Not file nor directory"));
-    }
 
-    targets
-        .iter()
-        .flatten()
-        .for_each(|target| println!("{target}"));
+            targets
+                .iter()
+                .flatten()
+                .for_each(|target| println!("{target}"));
+        }
+        _ => unimplemented!(),
+    }
 
     Ok(())
 }
